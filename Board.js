ef1b24a66ad62f4855e88e80f0a565bdbc8a2a23
D3STRON <ghoshanurag99@gmail.com>
2021-09-04 01:12:48 +0530

    added min max evaluation

--- Board.js
+++ Board.js
@@ -58,6 +58,64 @@ class Board{
         return pins;
     }
 
+    evaluate(playerType, column)
+    {
+        var row = this.height_of_column[column];
+        var score = 0;
+        if(row==this.size){
+
+            return -1*playerType*Infinity;
+        }
+        this.board_array.put(this.map_coordinates(row,column),0,playerType);
+        this.height_of_column[column]++;
+        if(Math.floor(this.size/2)==column)
+        {
+            score += 3;
+        }
+        score += this.check_minMax(row, column, 1, 1) + this.check_minMax(row,column,1,0) 
+                + this.check_minMax(row,column, -1, 1) + this.check_minMax(row,column,0, 1); 
+        // this.board_array.put(this.map_coordinates(row,column),0,0);
+        // this.height_of_column[column]--;
+        return score;
+    }
+
+    check_minMax(row, column, increment_row, increment_col)
+    {
+        var type =  this.board_array.get(this.map_coordinates(row,column),0);
+        var next_row = row;
+        var next_col = column;
+        var pins = -1;
+        while(next_row>=0 && next_row<this.size 
+            && next_col>=0 && next_col<this.size 
+            && this.board_array.get(this.map_coordinates(next_row,next_col),0)==type)
+        {
+            next_row += increment_row;
+            next_col += increment_col;
+        }
+        next_row -= increment_row;
+        next_col -= increment_col;
+        var continuous = true;
+        while(next_row>=0 && next_row<this.size 
+            && next_col>=0 && next_col<this.size 
+            && (this.board_array.get(this.map_coordinates(next_row,next_col),0)==type 
+            || this.board_array.get(this.map_coordinates(next_row,next_col),0)==0))
+        {
+            
+            if(this.board_array.get(this.map_coordinates(next_row,next_col),0)==type){
+                if (continuous && pins ==this.connect-1)
+                {
+                    return type*Infinity
+                }
+                pins+=1;
+            }
+            else{
+                continuous = false;
+            }
+            next_row -= increment_row;
+            next_col -= increment_col;  
+        }
+        return pins*2;
+    }
     map_coordinates(row, column)
     {
         return row*this.size + column;

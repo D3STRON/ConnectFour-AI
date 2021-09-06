54f34ceb79828512deb1754146a8e63b70a8a6a5
D3STRON <ghoshanurag99@gmail.com>
2021-09-06 10:58:51 +0530

    some refactoring

--- Board.js
+++ Board.js
@@ -4,13 +4,13 @@ class Board{
     {
         this.size = board_size;
         this.height_of_column = new Array(board_size).fill(0);
-        this.board_array = new Matrix(this.size*this.size,1)
-        this.connect = connect
-        this.display_size = this.size*this.size*10
+        this.board_array = new Matrix(this.size*this.size,1);
+        this.connect = connect;
+        this.display_size = this.size*this.size*10;
         this.unit_size = this.size*10
         this.padding = 50;
-        this.gap_point = 0.3
-        this.pin_point = 2
+        this.gap_point = 0.3;
+        this.pin_point = 2;
     }
 
     show()
@@ -42,16 +42,16 @@ class Board{
         var score_divisor = (Math.abs(Math.floor(this.size/2)-column)+1)
         var score = Math.floor(this.size/score_divisor);
         // check socre in horizontal vertical and diagonal directions
-        score += this.check_score(row, column, 1, 1, this.gap_point) 
-                    + this.check_score(row,column,1,0,0) 
-                        + this.check_score(row,column, -1, 1, this.gap_point) 
-                            + this.check_score(row,column,0, 1, this.gap_point); 
+        score += this.check_score(row, column, 1, 1) 
+                    + this.check_score(row,column,1,0) 
+                        + this.check_score(row,column, -1, 1) 
+                            + this.check_score(row,column,0, 1); 
         return score*playerType;
     }
 
-    check_score(row, column, increment_row, increment_col, gap_point)
+    check_score(row, column, increment_row, increment_col)
     {
-        var type =  this.board_array.get(this.map_coordinates(row,column),0);
+        var type =  this.get_pin_at(row,column);
         var next_row = row;
         var next_col = column;
         var pins = 0;
@@ -59,8 +59,8 @@ class Board{
         //this loop goes the extreme end of the line we are trying to check 
         while(next_row>=0 && next_row<this.size 
             && next_col>=0 && next_col<this.size 
-            && (this.board_array.get(this.map_coordinates(next_row,next_col),0)==type 
-            || this.board_array.get(this.map_coordinates(next_row,next_col),0)==0))
+            && (this.get_pin_at(next_row,next_col)==type 
+            || this.get_pin_at(next_row,next_col)==0))
         {
             next_row += increment_row;
             next_col += increment_col;
@@ -73,11 +73,11 @@ class Board{
         //now we count the pins
         while(next_row>=0 && next_row<this.size 
             && next_col>=0 && next_col<this.size 
-            && (this.board_array.get(this.map_coordinates(next_row,next_col),0)==type 
-            || this.board_array.get(this.map_coordinates(next_row,next_col),0)==0))
+            && (this.get_pin_at(next_row,next_col)==type 
+            || this.get_pin_at(next_row,next_col)==0))
         {
             
-            if(this.board_array.get(this.map_coordinates(next_row,next_col),0)==type){
+            if(this.get_pin_at(next_row,next_col)==type){
                 pins+=1;
                 continuous_pins+=1;
                 continuous = true;
@@ -87,7 +87,7 @@ class Board{
                 }
             }
             else{
-                if(this.board_array.get(this.map_coordinates(next_row,next_col),0)==0)
+                if(this.get_pin_at(next_row,next_col)==0)
                 {
                     gaps +=1;
                 }
@@ -104,6 +104,12 @@ class Board{
         }
         return 0
     }
+
+    get_pin_at(row, column)
+    {
+        return this.board_array.get(this.map_coordinates(row,column),0)
+    }
+
     map_coordinates(row, column)
     {
         //maps matrix coordinates to linear coordinates

9e85664c014fc09da896a985c0a4dabbb472164e
D3STRON <ghoshanurag99@gmail.com>
2021-09-11 12:12:09 +0530

    improvements

--- Player.js
+++ Player.js
@@ -9,17 +9,7 @@ class Player{
             this.pin_point_opponent = ParentPlayer.pin_point_opponent;
         }
         else{
-            //these evaluation point were brewed by genetic algo
-            //you can start with random values and reach these results
-            this.gap_point = 1;
-            this.pin_point = 10;
             this.center_point = [0,1,3.5,7,3.5,1,0];
-            this.pin_point_opponent = 5;
-            //values I started with
-            // this.gap_point = 0.3;
-            // this.pin_point = 2;
-            // this.center_point = 7;
-            // this.pin_point_opponent = 2;
         }
         this.fitness = 0;
         this.default_depth = 7;
@@ -45,23 +35,23 @@ class Player{
 
         if(Math.random()<rate)
         {
-            this.gap_point += randomGaussian(0, 0.1)
-            console.log(this.gap_point)
+            this.center_point[0] += Math.round(randomGaussian(0, 1));
+            console.log(this.center_point[0]);
         }
         if(Math.random()<rate)
         {
-            this.pin_point += Math.round(randomGaussian(0, 1));
-            console.log(this.pin_point)
+            this.center_point[1] += Math.round(randomGaussian(0, 1));
+            console.log(this.center_point[1]);
         }
         if(Math.random()<rate)
         {
-            this.center_point += Math.round(randomGaussian(0, 1));
-            console.log(this.center_point)
+            this.center_point[2] += Math.round(randomGaussian(0, 1));
+            console.log(this.center_point[2]);
         }
         if(Math.random()<rate)
         {
-            this.pin_point_opponent += Math.round(randomGaussian(0, 1));
-            console.log(this.pin_point_opponent)
+            this.center_point[3] += Math.round(randomGaussian(0, 1));
+            console.log(this.center_point[3]);
         }
     }
 
@@ -198,11 +188,12 @@ class Player{
             var right = i+1;
             var row = board.height_of_column[i];
             while(
-                    (left>=0 && row>=0 && row<board_vertical_size && board.get_pin_at(row,left)!=0)
-                ||  (right<board.size && row>=0 && row<board_vertical_size && board.get_pin_at(row,right)!=0)
-                ||  (right<board.size && row>0 && row<board_vertical_size && board.get_pin_at(row-1,right)!=0)
-                ||  (left>=0 && row>0 && row<board_vertical_size && board.get_pin_at(row-1,left)!=0)
-                ||  (row>0 && row<board_vertical_size && board.get_pin_at(row-1,i)!=0)
+                row>0 && row<board.size &&
+                    ((left>=0 && board.get_pin_at(row,left)!=0)
+                ||  (right<board.size && board.get_pin_at(row,right)!=0)
+                ||  (right<board.size && board.get_pin_at(row-1,right)!=0)
+                ||  (left>=0 && board.get_pin_at(row-1,left)!=0)
+                ||  (board.get_pin_at(row-1,i)!=0))
             )
             {
                 score += this.check_linear_evaluation(board, row, i, 0, 1);
